51e8a511bdab5c67f261caea6164174b02e311b9
Looo911 <lo.gunreben@gmail.com>
2020-03-26 18:16:33 +0200

    first commit

---
+++ Models/RequestManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandscapeAppWebsite.Models
+{
+    public class RequestManager
+    {
+        public int LorX { get; set; }
+        public int LorY { get; set; }
+
+        public int CompSizeX { get; set; }
+
+        public int CompSizeY { get; set; }
+
+        public int NumComponentsX { get; set; }
+
+        public int NumComponentsY { get; set; }
+
+        public int CoordX { get; set; }
+
+        public int CoordY { get; set; }
+
+        public String ServerName { get; set; }
+
+        public String Version { get; set; }
+
+        public int NativePictureResX { get; set; }
+
+        public int NativePictureResY { get; set; }
+
+        public float DOPRes { get; set; }
+
+        public void UpdateValues()
+        {
+            CalcComponentSize();
+            CalcNativePictureResolution();
+            UpdateURLParts();
+        }
+
+        private void UpdateURLParts()
+        {
+           
+        }
+
+        private void CalcNativePictureResolution()
+        {
+            //To Int correct?
+            NativePictureResX = (int)(CompSizeX / DOPRes);
+            NativePictureResY = (int)(CompSizeY / DOPRes);
+        }
+
+        private void CalcComponentSize()
+        {
+            CompSizeX = (LorX - 1) / NumComponentsX;
+            CompSizeY = (LorY - 1) / NumComponentsY;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LandscapeAppWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LandscapeAppWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        [BindProperty]
+        public RequestManager RM { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string CurrentLink { get; set; }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(CurrentLink))
+            {
+                CurrentLink = "Not defined";
+            }
+        }
+
+        //typical output of post:
+        public IActionResult OnPost()
+        {
+            //Check whether input was correct
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index", new { CurrentLink = JoinLink() });//, new { RM.LorX });
+        }
+
+        private String JoinLink()
+        {
+            return RM.CoordX.ToString();
+        }
+    }
+}
